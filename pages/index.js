94ee5b8244eb0f9f973c255a3605b11aa2ba1f68
lihaan <45350323+lihaan@users.noreply.github.com>
2019-02-05 04:54:19 +0800

    audio autoplay added

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,22 @@
-export default () => <div>Hello World! You ain't see nothing yet!LOLO</div>
+// This is the Link API
+import Link from 'next/link'
+
+
+const Index = () => (
+  <div>Hello World! You ain't see nothing yet! Except
+    <iframe src="/static/silence.mp3" allow="autoplay" id="audio" style={{display:'none'}}></iframe>
+    <div>
+    <audio autoPlay>
+  <source src="/static/anthem.mp3" type="audio/mpeg" />
+Your browser does not support the audio element.
+</audio>
+    </div>
+    <p> 
+      <Link href="/about">
+        <button style={{ fontSize: 20 }}>About Page</button>
+      </Link>
+    </p>
+  </div>
+)
+
+export default Index
